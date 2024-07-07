43ab124f893e49a87d0ee33e0aae67a022621dd4
Karan <karannarode999@gmail.com>
2024-07-07 23:41:16 +0530

    intial commit

--- 10react-routers/src/components/Footer/Footer.jsx
+++ 10react-routers/src/components/Footer/Footer.jsx
@@ -8,11 +8,7 @@ export default function Footer() {
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
-                            <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
-                                alt="Logo"
-                            />
+                            <h1 className='text-4xl font-thin'>KarΔn.ai</h1>
                         </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -36,7 +32,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/ikarannarode"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -73,7 +69,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © {date.getFullYear()}&nbsp;
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            karannarode
+                            <h1 className='font-thin inline-block'>KarΔn.ai</h1>
                         </a>
                         . All Rights Reserved.
                     </span>
--- 10react-routers/src/components/Header/Header.jsx
+++ 10react-routers/src/components/Header/Header.jsx
@@ -3,15 +3,14 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
     return (
+
+
+
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
-                            alt="Logo"
-                        />
+                        <h1 className='text-4xl font-thin'>KarΔn.ai</h1>
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
@@ -22,7 +21,7 @@ export default function Header() {
                         </Link>
                         <Link
                             to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
                         </Link>
