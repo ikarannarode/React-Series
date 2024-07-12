dede3f2f1def10aedcd1e11437c378ec26c25fe2
Karan <karannarode999@gmail.com>
2024-07-12 23:30:15 +0530

    main

--- 11context-api/src/App.jsx
+++ 11context-api/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-
+import UserContextProvider from "../src/context/UserContextProvider"
+import { Login } from "./components/Login"
+import { Profile } from './components/Profile'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <UserContextProvider>
+      <h1 className='text-cyan-800 text-5xl outline-blue-500'>Hello Moto</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider >
   )
 }
-
-export default App
+export default App;
